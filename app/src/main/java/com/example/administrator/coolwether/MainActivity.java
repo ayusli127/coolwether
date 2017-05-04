8f26e4b4c9a40ddcbd2f267f525a62a97bdc7253
ayusli127 <ayusli127@gmail.com>
2017-05-04 17:51:02 +0800

    finish city

--- app/src/main/java/com/example/administrator/coolwether/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolwether/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.coolwether;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SQLiteDatabase db = LitePal.getDatabase();
+        Log.d("MainActivity", "onCreate: "+db);
     }
 }
